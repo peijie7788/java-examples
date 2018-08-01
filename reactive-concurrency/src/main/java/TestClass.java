17b2afb56b8f44ec357ac41c3c998c43ec3ff63e
Peijie Liu <peijie.liu@kpit.com>
2018-08-01 10:07:30 +0200

    update gitignore

--- reactive-concurrency/src/main/java/TestClass.java
+++ reactive-concurrency/src/main/java/TestClass.java
@@ -1,5 +1,33 @@
+import java.util.concurrent.*;
+
 public class TestClass {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+        Callable<String> callable = () -> {
+            // Perform some computation
+            System.out.println("Entered Callable");
+            Thread.sleep(2000);
+            return "Hello from Callable";
+        };
+
+        System.out.println("Submitting Callable");
+        Future<String> future = executorService.submit(callable);
+
+        // This line executes immediately
+        System.out.println("Do something else while callable is getting executed");
+
+        System.out.println("Retrieve the result of the future");
+        // Future.get() blocks until the result is available
+
+        while(!future.isDone()){
+            System.out.println("Waiting");
+            Thread.sleep(200);
+        }
+
+        String result = future.get();
+        System.out.println(result);
+
+        executorService.shutdown();
     }
 }
